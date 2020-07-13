09c009e42b37d7f085ed92e2987cc5c5e31128ba
Peter Gemmell <petergemmell65@hotmail.com>
2020-07-13 11:10:16 +0100

    customer controller created

--- server/rentalservice/src/main/java/com/example/rentalsystem/rentalservice/controllers/CustomerController.java
+++ server/rentalservice/src/main/java/com/example/rentalsystem/rentalservice/controllers/CustomerController.java
@@ -1,4 +1,53 @@
 package com.example.rentalsystem.rentalservice.controllers;
 
+import com.example.rentalsystem.rentalservice.models.Customer;
+import com.example.rentalsystem.rentalservice.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer(@PathVariable Long id){
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/login")
+    public ResponseEntity findByCustomerUserNameAndPassword(
+            @RequestParam(name = "customerUserName") String customerUserName,
+            @RequestParam(name = "customerPassword") String customerPassword){
+        return new ResponseEntity(customerRepository.findByCustomerUserNameAndPassword(customerUserName, customerPassword), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/customers/{id}")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/customers/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id){
+        Customer found = customerRepository.getOne(id);
+        customerRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
